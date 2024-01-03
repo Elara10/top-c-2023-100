fef9f4610b3362e1baff58635bc20fe86770735b
IshaKumari10 <isha02740@gmail.com>
2024-01-03 19:23:24 +0530

    ASCII -65

---
+++ _4ASCII.cpp
@@ -0,0 +1,10 @@
+//4. Write a Program to Find the ASCII Value of a Character
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c='A';
+    cout<<"The ASCII value of "<<c<<" is "<<int(c);
+    return 0;
+}
