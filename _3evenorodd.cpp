e2f16292e3445c391ff76c52d1bc3a4b6f92d324
IshaKumari10 <isha02740@gmail.com>
2024-01-03 19:15:24 +0530

    even and odd

---
+++ _3evenorodd.cpp
@@ -0,0 +1,15 @@
+//3. C++ Program To Check Whether Number is Even Or Odd
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=24;
+    if(n%2==0)
+    {
+        cout<<n<<" is even number";
+    }
+    else{
+        cout<<n<<" is odd number";
+    }
+}
